cb4cf50825f695968f8862fa6d6c5be11c5ced0f
unknown <Leudrit.ajeti@student.cacttus.education>
2025-07-22 18:45:55 +0200

    Fix port binding for Render

--- server/index.ts
+++ server/index.ts
@@ -59,9 +59,7 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 5000;
-  server.listen(port, "localhost", () => {
-    log(`serving on http://localhost:${port}`);
-  });
+  const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   
 })();
